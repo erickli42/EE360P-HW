dd56830eb89b30c39849057ec4781ecee29a8101
RoyceHong <rhchen73@utexas.edu>
2021-02-25 00:02:54 -0600

    Update testPriorityQueue.java

--- HW2/testPriorityQueue.java
+++ HW2/testPriorityQueue.java
@@ -1,42 +1,41 @@
+import java.util.Random;
+
 public class testPriorityQueue implements Runnable {
-    final static int SIZE = 5;
-    final static int ROUND = 5;
+    final static int Q_SIZE = 10;
+    final static int T_SIZE = 5;
 
-    //	final CyclicBarrier gate;
-    final MonitorCyclicBarrier gate;
+    final PriorityQueue queue;
 
-    //	public testCyclicBarrier(CyclicBarrier gate) {
-//		this.gate = gate;
-//	}
-    public testPriorityQueue(MonitorCyclicBarrier gate) {
-        this.gate = gate;
+    public testPriorityQueue(PriorityQueue queue) {
+        this.queue = queue;
     }
 
     public void run() {
-        int index = -1;
-
-        for (int round = 0; round < ROUND; ++round) {
-            System.out.println("Thread " + Thread.currentThread().getId() + " is WAITING round:" + round);
-            try {
-                index = gate.await();
-//				System.out.println("Thread " + Thread.currentThread().getId() + " is index:" + index);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+        while(true) {
+            Random rand = new Random();
+            int rand_int = rand.nextInt(1000);
+            if (rand_int < 500) {
+                int pri = rand.nextInt(10);
+                String name = "sam" + pri;
+                queue.add(name, pri);
+                System.out.println("Added " + name);
+            } else {
+                String pop =  queue.getFirst();
+                System.out.println("Popped " + pop);
             }
-            System.out.println("Thread " + Thread.currentThread().getId() + " is leaving round:" + round);
         }
+
     }
 
     public static void main(String[] args) {
-//		CyclicBarrier gate = new CyclicBarrier(SIZE);
-        MonitorCyclicBarrier gate = new MonitorCyclicBarrier(SIZE);
-        Thread[] t = new Thread[SIZE];
+        PriorityQueue queue = new PriorityQueue(Q_SIZE);
+        Thread[] t = new Thread[T_SIZE];
 
-        for (int i = 0; i < SIZE; ++i) {
-            t[i] = new Thread(new testCyclicBarrier(gate));
+        for (int i = 0; i < T_SIZE; ++i) {
+            t[i] = new Thread(new testPriorityQueue(queue));
         }
 
-        for (int i = 0; i < SIZE; ++i) {
+        for (int i = 0; i < T_SIZE; ++i) {
             t[i].start();
         }
     }
